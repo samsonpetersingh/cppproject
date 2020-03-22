345078086ce6c8d7e3fa4f464045f53cc300a695
T Samson <samsonps@gmail.com>
2020-03-22 16:13:43 +0530

    added cpp file

--- cppproject/cppproject.cpp
+++ cppproject/cppproject.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+int functiontoadd(int& value1,int& value2);
 
 int main()
 {
-    return 0;
+	int val1=100;
+	int val2=200;
+	int sum=functiontoadd(val1, val2);
+	std::cout << "Added Value: " <<sum<< std::endl;
+
+	return 0;
 }
+
+int functiontoadd(int& value1, int& value2)
+{
+	return value1 + value2;
+}
+
+
+
 
